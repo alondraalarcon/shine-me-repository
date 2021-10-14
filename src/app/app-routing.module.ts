420a5e37787d5f40606a9703e722eef2cb84a28d
unknown <alondraalarcon0528@gmail.com>
2021-10-14 13:30:16 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RiderComponent } from './rider/rider.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'rider', component: RiderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RiderComponent } from './rider/rider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RiderComponent
   ],
   imports: [
     BrowserModule,
